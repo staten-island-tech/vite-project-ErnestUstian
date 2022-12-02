82f17746dcd3e78fc3c7d28123befb6f110de35c
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-02 14:01:48 -0500

    vmbhj

---
+++ vite-project/Food.js
@@ -0,0 +1,24 @@
+
+const food = [
+    {
+    name: "Khinkali",
+    price: 1.25,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Khinkali_551.jpg/220px-Khinkali_551.jpg",
+    nationality: "Georgian"
+},
+{
+    name: "Adjaruli",
+    price: 10,
+    img: "https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_AdjaruliKhachapuriReshoot_008_pjflzv",
+    group: "Khachapuri",
+    nationality: "Georgian"
+},
+{
+    name: "Imeruli",
+    price: 3.5,
+    img: "https://img.freepik.com/free-photo/georgian-cheese-khachapuri-imeruli-georgian-traditional-food-hot-khachapuri_114579-210.jpg?w=740&t=st=1670007306~exp=1670007906~hmac=a01547ebf7cb4bfd05189101ad47fef90dfb83b7cf5ed36fc3e5e184e5529590",
+    group: "Khachapuri",
+    nationality: "Georgian"
+},
+
+]
