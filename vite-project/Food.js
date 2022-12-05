9a1b991b7c64983cc08d2c83099caf8c6843d238
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-05 14:01:11 -0500

    nf

--- vite-project/Food.js
+++ vite-project/Food.js
@@ -20,5 +20,47 @@ const food = [
     group: "Khachapuri",
     nationality: "Georgian"
 },
-
+{
+    name: "Penovani",
+    price: 5,
+    img: "https://www.tasteatlas.com/images/recipes/5cbd5ee7a9184c03a90f6356e943a7ce.jpg?mw=910",
+    group: "Khachapuri",
+    nationality: "Georgian"
+},
+{
+    name: "Armenian Dolma",
+    price: 0.75,
+    img: "https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2Fu-P974Tif09-G-JhB38RDFqZ95w%3D%2F864x648%2Fimages.kitchenstories.io%2FcommunityImages%2F9478b9ca14a6160a84d2e75f779ba6a8_01d8c797-1edb-4386-a03d-05a6d6d4c15f.jpg&w=1024&q=90",
+    nationality: "Armenian"
+},
+{
+    name: "Shashlik",
+    price: 15,
+    img: "https://thumbs.dreamstime.com/z/concept-georgian-cuisine-meat-board-shashlik-grilled-pork-ribs-lulya-kebab-shish-french-fries-vegetables-feed-139705680.jpg",
+    nationality: "Georgian"
+},
+{
+    name: "Pkhali",
+    price: 8,
+    img: "https://nofrillskitchen.com/wp-content/uploads/2020/12/DSC09285.jpg",
+    nationality: "Georgian"
+},
+{
+    name: "Churchkhela",
+    price: 6.50,
+    img: "https://i5.walmartimages.com/asr/81f23074-0a69-4d6c-88b7-76d3f16e8efb.1409d91b1b3cedc3207aef3532190db4.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    nationality: "Georgian"
+},
+{
+    name: "Gozinaki",
+    price: 10,
+    img: "https://georgianjournal.ge/media/_thumb/images/georgianews/2018/December/NewYearDinner/gozinaki.jpg",
+    nationality: "Georgian"
+},
+{
+    name: "Baklava",
+    price: 5,
+    img: "https://www.196flavors.com/wp-content/uploads/2016/05/tenbel-pakhlava-2.jpg",
+    nationality: "Armenian"
+}
 ]
