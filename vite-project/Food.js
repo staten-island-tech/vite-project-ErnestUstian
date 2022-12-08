8eacb1d41292b91eaf8715fcf4768f4cf60af3e2
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-08 14:01:05 -0500

    fgh

--- vite-project/Food.js
+++ vite-project/Food.js
@@ -4,70 +4,96 @@ const food = [
     name: "Khinkali",
     price: 1.25,
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Khinkali_551.jpg/220px-Khinkali_551.jpg",
-    nationality: "Georgian"
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Adjaruli",
     price: 10,
     img: "https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_AdjaruliKhachapuriReshoot_008_pjflzv",
-    group: "Khachapuri",
-    nationality: "Georgian"
+    type: "Khachapuri",
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Imeruli",
     price: 3.5,
     img: "https://img.freepik.com/free-photo/georgian-cheese-khachapuri-imeruli-georgian-traditional-food-hot-khachapuri_114579-210.jpg?w=740&t=st=1670007306~exp=1670007906~hmac=a01547ebf7cb4bfd05189101ad47fef90dfb83b7cf5ed36fc3e5e184e5529590",
-    group: "Khachapuri",
-    nationality: "Georgian"
+    type: "Khachapuri",
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Penovani",
     price: 5,
     img: "https://www.tasteatlas.com/images/recipes/5cbd5ee7a9184c03a90f6356e943a7ce.jpg?mw=910",
-    group: "Khachapuri",
-    nationality: "Georgian"
+    type: "Khachapuri",
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Armenian Dolma",
     price: 0.75,
     img: "https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2Fu-P974Tif09-G-JhB38RDFqZ95w%3D%2F864x648%2Fimages.kitchenstories.io%2FcommunityImages%2F9478b9ca14a6160a84d2e75f779ba6a8_01d8c797-1edb-4386-a03d-05a6d6d4c15f.jpg&w=1024&q=90",
+    group: "food",
     nationality: "Armenian"
 },
 {
     name: "Shashlik",
     price: 15,
     img: "https://thumbs.dreamstime.com/z/concept-georgian-cuisine-meat-board-shashlik-grilled-pork-ribs-lulya-kebab-shish-french-fries-vegetables-feed-139705680.jpg",
-    nationality: "Georgian"
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Pkhali",
     price: 8,
     img: "https://nofrillskitchen.com/wp-content/uploads/2020/12/DSC09285.jpg",
-    nationality: "Georgian"
+    group: "food",
+    nationality: "georgian"
 },
 {
     name: "Churchkhela",
     price: 6.50,
     img: "https://i5.walmartimages.com/asr/81f23074-0a69-4d6c-88b7-76d3f16e8efb.1409d91b1b3cedc3207aef3532190db4.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
-    nationality: "Georgian"
+    group: "sweet",
+    nationality: "georgian"
 },
 {
     name: "Gozinaki",
     price: 10,
     img: "https://georgianjournal.ge/media/_thumb/images/georgianews/2018/December/NewYearDinner/gozinaki.jpg",
-    nationality: "Georgian"
+    group: "sweet",
+    nationality: "georgian"
 },
 {
     name: "Gata",
     price: 5,
     img: "https://mission-food.com/wp-content/uploads/2015/12/Gata-24.jpg",
+    group: "sweet",
     nationality: "Armenian"
 },
 {
     name: "Lavash",
     price: 3,
     img: "https://assets.epicurious.com/photos/5df942b3f7763a0008ad91b9/1:1/w_1920,c_limit/lavash-recipe-121719.jpg",
+    group: "food",
     nationality: "Armenian"
+
+},
+{
+    name: "Tarragon Lemonade",
+    price: 2.5,
+    img: "http://img.moldovastore.ie/images/products/name2654.jpg",
+    group: "drink",
+    nationality: "Georgian"
+},
+{
+    name: "Pear Lemonade",
+    price: 2.5,
+    img: "data:image/gif;base64,R0lGODlhZABkAPABAHOf4fj48yH5BAEAAAEALAAAAABkAGQAAAL+jI+py+0PowOB2oqvznz7Dn5iSI7SiabqWrbj68bwTLL2jUv0Lvf8X8sJhzmg0Yc8mojM5kmZjEKPzqp1MZVqs7Cr98rdisOXr7lJHquz57YwDV8j3XRb/C7v1vcovD8PwicY8VcISDGY2GDIKKf4mNAoKQZZeXg5aQk5yRml+dgZ2vOpKGraQpp4uhqYKsgKi+H6iln7N8sXG4u7p2s7ykvnyxos/DuMWtyGfKq8fAwd5nzGHN067VUtiv2lbV3GDfY9DhQu7p1pXoU+rr5ODk/j7sSePk9Ub33PlN+4jx8v4JJ/RQQa3EDwzcGFiBLi6AfN4UOGCyXegGjIoh0fisQ0rsD4y+NHjgZFqgB5y2Qfks1UPmEZ0OVLlIcKAAA7",
+    group: "drink",
+    nationality: "Georgian"
 }
 ]
 export {food}
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,1 @@
 import { food } from "./Food";
-
