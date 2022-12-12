daaa8a40143a83758cbf0ad61a7ac9730188e36c
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-12 14:03:05 -0500

    hgfjj

---
+++ vite-project/DOM.js
@@ -0,0 +1,11 @@
+const DOM = {
+    MainMenu: document.getElementById("Main Menu"),
+    Georgian: document.getElementById("Georgian"),
+    Armenian: document.getElementById("Armenian"),
+    Khachapuri: document.getElementById("Khachapuri"),
+    food: document.getElementById("food"),
+    sweet: document.getElementById("sweet"),
+    drinku: document.getElementById("drink")
+}
+
+export {DOM}
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,14 @@
+import "./style.css";
 import { food } from "./Food";
+import {DOM} from "./DOM";
+
+const AllFunctions = {
+    MainMenu: function () {
+        DOM.MainMenu.insertAdjacentElement(
+            "beforeend", 
+            `
+            `
+
+        )
+    }
+}
