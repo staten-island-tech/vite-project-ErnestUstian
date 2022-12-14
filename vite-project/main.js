d292aa67f302491a18fd13c0992bff3e93fc1a9b
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-14 14:04:26 -0500

    iuop

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,13 +1,13 @@
-import "./style.css";
 import { food } from "./Food";
 import {DOM} from "./DOM";
 
 
 DOM.MainMenu.addEventListener("click", function(){
     food.forEach((food) => DOM.Menupage.insertAdjacentHTML("beforeend", `
-    <div>
-     <img src="${food.img}"><img>
-     <h1>${food.price}</h1>
+    <div class="card">
+        <h1>${food.name}</h1>
+        <h2>${food.price}</h2>
+        <img src="${food.img}">
     </div>
     `))
    
