418f0ff934a97b0293f4e60b1434ab9c64cae444
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-12-16 14:04:00 -0500

    hjikul;

--- vite-project/Food.js
+++ vite-project/Food.js
@@ -5,13 +5,14 @@ const food = [
     price: 1.25,
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Khinkali_551.jpg/220px-Khinkali_551.jpg",
     group: "food",
+    type: "",
     nationality: "georgian"
 },
 {
     name: "Adjaruli",
     price: 10,
     img: "https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_AdjaruliKhachapuriReshoot_008_pjflzv",
-    type: "Khachapuri",
+    type: "khachapuri",
     group: "food",
     nationality: "georgian"
 },
@@ -19,7 +20,7 @@ const food = [
     name: "Imeruli",
     price: 3.5,
     img: "https://img.freepik.com/free-photo/georgian-cheese-khachapuri-imeruli-georgian-traditional-food-hot-khachapuri_114579-210.jpg?w=740&t=st=1670007306~exp=1670007906~hmac=a01547ebf7cb4bfd05189101ad47fef90dfb83b7cf5ed36fc3e5e184e5529590",
-    type: "Khachapuri",
+    type: "khachapuri",
     group: "food",
     nationality: "georgian"
 },
@@ -27,7 +28,7 @@ const food = [
     name: "Penovani",
     price: 5,
     img: "https://www.tasteatlas.com/images/recipes/5cbd5ee7a9184c03a90f6356e943a7ce.jpg?mw=910",
-    type: "Khachapuri",
+    type: "khachapuri",
     group: "food",
     nationality: "georgian"
 },
@@ -35,13 +36,15 @@ const food = [
     name: "Armenian Dolma",
     price: 0.75,
     img: "https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2Fu-P974Tif09-G-JhB38RDFqZ95w%3D%2F864x648%2Fimages.kitchenstories.io%2FcommunityImages%2F9478b9ca14a6160a84d2e75f779ba6a8_01d8c797-1edb-4386-a03d-05a6d6d4c15f.jpg&w=1024&q=90",
+    type: "",
     group: "food",
-    nationality: "Armenian"
+    nationality: "armenian"
 },
 {
     name: "Shashlik",
     price: 15,
     img: "https://thumbs.dreamstime.com/z/concept-georgian-cuisine-meat-board-shashlik-grilled-pork-ribs-lulya-kebab-shish-french-fries-vegetables-feed-139705680.jpg",
+    type: "",
     group: "food",
     nationality: "georgian"
 },
@@ -49,6 +52,7 @@ const food = [
     name: "Pkhali",
     price: 8,
     img: "https://nofrillskitchen.com/wp-content/uploads/2020/12/DSC09285.jpg",
+    type: "",
     group: "food",
     nationality: "georgian"
 },
@@ -56,6 +60,7 @@ const food = [
     name: "Churchkhela",
     price: 6.50,
     img: "https://i5.walmartimages.com/asr/81f23074-0a69-4d6c-88b7-76d3f16e8efb.1409d91b1b3cedc3207aef3532190db4.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    type: "",
     group: "sweet",
     nationality: "georgian"
 },
@@ -63,6 +68,7 @@ const food = [
     name: "Gozinaki",
     price: 10,
     img: "https://georgianjournal.ge/media/_thumb/images/georgianews/2018/December/NewYearDinner/gozinaki.jpg",
+    type: "",
     group: "sweet",
     nationality: "georgian"
 },
@@ -70,30 +76,34 @@ const food = [
     name: "Gata",
     price: 5,
     img: "https://mission-food.com/wp-content/uploads/2015/12/Gata-24.jpg",
+    type: "",
     group: "sweet",
-    nationality: "Armenian"
+    nationality: "armenian"
 },
 {
     name: "Lavash",
     price: 3,
     img: "https://assets.epicurious.com/photos/5df942b3f7763a0008ad91b9/1:1/w_1920,c_limit/lavash-recipe-121719.jpg",
+    type: "",
     group: "food",
-    nationality: "Armenian"
+    nationality: "armenian"
 
 },
 {
     name: "Tarragon Soda",
     price: 2.5,
     img: "http://img.moldovastore.ie/images/products/name2654.jpg",
+    type: "",
     group: "drink",
-    nationality: "Georgian"
+    nationality: "georgian"
 },
 {
     name: "Pear Soda",
     price: 2.5,
+    type: "",
     img: "https://alfaaltrading.com/web/image/product.product/2338/image_1024/Natakhtari%20Georgian%20Lemonade%20Pear%20Flavor%200.5L?unique=1f110ca",
     group: "drink",
-    nationality: "Georgian"
+    nationality: "georgian"
 }
 ]
 export {food}
--- vite-project/main.js
+++ vite-project/main.js
@@ -3,13 +3,45 @@ import {DOM} from "./DOM";
 
 
 DOM.MainMenu.addEventListener("click", function(){
+    DOM.Menupage.innerHTML = "";
     food.forEach((food) => DOM.Menupage.insertAdjacentHTML("beforeend", `
     <div class="card">
         <h1>${food.name}</h1>
-        <h2>${food.price}</h2>
+        <h2>$${food.price}</h2>
         <img src="${food.img}">
     </div>
     `))
-   
-    
+})
+DOM.Georgian.addEventListener("click", function(){
+    DOM.Menupage.innerHTML = "";
+    food.filter((food) => food.nationality.includes("georgian"))
+    .forEach((food) => DOM.Menupage.insertAdjacentHTML("beforeend", `
+    <div class="card">
+        <h1>${food.name}</h1>
+        <h2>$${food.price}</h2>
+        <img src="${food.img}">
+    </div>
+    `))
+})
+DOM.Armenian.addEventListener("click", function(){
+    DOM.Menupage.innerHTML = "";
+    food.filter((food) => food.nationality.includes("armenian"))
+    .forEach((food) => DOM.Menupage.insertAdjacentHTML("beforeend", `
+    <div class="card">
+        <h1>${food.name}</h1>
+        <h2>$${food.price}</h2>
+        <img src="${food.img}">
+    </div>
+    `))
+})
+DOM.Khachapuri.addEventListener("click", function(){
+    DOM.Menupage.innerHTML = "";
+    food.filter((food) => food.type.includes("khachapuri"))
+    .forEach((food) => DOM.Menupage.insertAdjacentHTML("beforeend", `
+    <div class="card">
+        <h1>${food.name}</h1>
+        <h2>$${food.price}</h2>
+        <img src="${food.img}">
+    </div>
+    `))
 })
