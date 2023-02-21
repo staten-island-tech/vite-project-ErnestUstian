4a27515b3a89b765df3231d0e904f1c0154d8a7b
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-02-21 18:14:05 -0500

    gfg

--- vite-project/DOM.js
+++ vite-project/DOM.js
@@ -1,13 +1,13 @@
 const DOM = {
-    MainMenu: document.getElementById("MainMenu"),
-    Georgian: document.getElementById("Georgian"),
-    Armenian: document.getElementById("Armenian"),
-    Khachapuri: document.getElementById("Khachapuri"),
-    food: document.getElementById("food"),
-    sweet: document.getElementById("sweet"),
-    drink: document.getElementById("drink"),
-    Menupage: document.getElementById("Menupage"),
-    Warm: document.getElementById("Warm"),
-}
+  MainMenu: document.getElementById("MainMenu"),
+  Georgian: document.getElementById("Georgian"),
+  Armenian: document.getElementById("Armenian"),
+  Khachapuri: document.getElementById("Khachapuri"),
+  food: document.getElementById("food"),
+  sweet: document.getElementById("sweet"),
+  drink: document.getElementById("drink"),
+  Menupage: document.getElementById("Menupage"),
+  Warm: document.getElementById("Warm"),
+};
 
-export {DOM}
+export { DOM };
